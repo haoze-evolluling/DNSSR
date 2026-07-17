34d99a02ff52cc2f12afdd13a00409e8970e7486
lee <leehaoze2022@gmail.com>
2026-07-17 08:37:41 +0800

    build(app): 升级版本号至2.50
    将versionCode更新为25000，versionName更新为2.50

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.haoze.dnssr"
         minSdk = 24
         targetSdk = 36
-        versionCode = 24500
-        versionName = "2.45"
+        versionCode = 25000
+        versionName = "2.50"
         ndk {
             abiFilters += "arm64-v8a"
         }
